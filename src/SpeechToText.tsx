90326f50458bf5a278282411a8d78877135cb570
Truong Tran <truong.tran0813@gmail.com>
2022-03-27 17:06:58 +0700

    add speech to text

---
+++ src/SpeechToText.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import { FloatingLabel, Form, Spinner } from "react-bootstrap";
+
+export const SpeechToText = () => {
+  const [loadingFile, setLoadingFile] = useState(false);
+  const [text, setText] = useState<string[]>([]);
+  const token_id = process.env.REACT_APP_TOKEN_ID || "";
+  const url = "https://viettelgroup.ai/voice/api/asr/v1/rest/decode_file";
+
+  const handleSetFile = async (file: File) => {
+    if (file.type !== "audio/mpeg") {
+      alert("Vui lòng chọn file âm thanh");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", file);
+    setLoadingFile(true);
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        //"Content-Type": "multipart/form-data; boundary=----WebKitFormBoundary",
+        token: token_id,
+      },
+      body: formData,
+    }).then((res) => res.json());
+    setLoadingFile(false);
+    if (response && response.length > 0) {
+      let newText: string[] = [];
+      response.forEach((e: any) => {
+        let result = e.result.hypotheses;
+
+        result.forEach((v: any) => {
+          newText.push(v.transcript);
+        });
+      });
+      setText(newText);
+    }
+  };
+  console.log(text);
+  return (
+    <>
+      <h4 className="mb-4">
+        Website dựa trên{" "}
+        <a
+          href="https://viettelgroup.ai/service/asr"
+          rel="noreferrer"
+          target="_blank"
+        >
+          ViettelAI (API ASR - Speech To Text)
+        </a>
+      </h4>
+      <div className="my-3">
+        <label className="btn btn-primary">
+          {loadingFile ? (
+            <Spinner animation="border" variant="light" />
+          ) : (
+            `Tải file`
+          )}
+          <input
+            type="file"
+            className="d-none"
+            onChange={(e) => e.target.files && handleSetFile(e.target.files[0])}
+          />
+        </label>
+      </div>
+      <div
+        className="asr-editor"
+        placeholder="Ghi âm trực tiếp, tải file ghi âm sẵn hoặc sử dụng file mẫu để trải nghiệm"
+      >
+        {text.map((t) => (
+          <>
+            {t}
+            <br />
+          </>
+        ))}
+      </div>
+    </>
+  );
+};
---
+++ src/TextToSpeech.tsx
@@ -0,0 +1,199 @@
+import { useEffect, useRef, useState } from "react";
+import { FloatingLabel, Form, Spinner } from "react-bootstrap";
+import "./App.css";
+
+export interface IVoices {
+  name: string;
+  description: string;
+  code: string;
+}
+export const TextToSpeech = () => {
+  const [text, setText] = useState("");
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [voices, setVoices] = useState<IVoices[]>([]);
+  const [voice, setVoice] = useState("");
+  const [speed, setSpeed] = useState(1);
+  const [isLoading, setLoading] = useState(false);
+  const token_id = process.env.REACT_APP_TOKEN_ID || "";
+  //Để có "token id" bạn có thể đăng ký tài khoản tại viettelgroup.ai, sau đó login, rồi vào menu token để tạo
+
+  const url = "https://viettelgroup.ai/voice/api/tts/v1/rest/syn";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch("https://viettelgroup.ai/voice/api/tts/v1/rest/voices")
+        .then((response) => response.json())
+        .then((data) => {
+          setVoices(data);
+          setVoice(data[0].code);
+        });
+    };
+    fetchData();
+  }, []);
+
+  const getData = async () => {
+    const data = {
+      text: text,
+      voice,
+      id: "3",
+      without_filter: false,
+      speed: speed,
+      tts_return_option: 3,
+    };
+
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        token: token_id,
+      },
+      body: JSON.stringify(data),
+    });
+    return response;
+  };
+
+  const convert = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!text) {
+      alert("Vui lòng nhập nội dung muốn chuyển đổi");
+      return;
+    }
+    if (text.length > 500) {
+      alert("Nội dung không quá 500 ký tự");
+      return;
+    }
+    setLoading(true);
+    const data = await getData();
+    if (data.body) {
+      const reader = data.body.getReader();
+      let arr: any[] = [];
+      let result;
+      while (!(result = await reader.read()).done) {
+        arr.push(result.value);
+      }
+      const newArr = new Uint8Array(
+        arr.reduce((acc, curr) => [...acc, ...curr], [])
+      );
+      const blob = new Blob([newArr], { type: "audio/mp3" });
+      let url = window.URL.createObjectURL(blob);
+      console.log(url);
+      if (audioRef.current) {
+        audioRef.current.src = url;
+        audioRef.current.play();
+      }
+    }
+    setLoading(false);
+  };
+  const randomName = () => {
+    var m = new Date();
+    var dateString =
+      m.getUTCFullYear() +
+      ("0" + (m.getUTCMonth() + 1)).slice(-2) +
+      ("0" + m.getUTCDate()).slice(-2) +
+      ("0" + m.getUTCHours()).slice(-2) +
+      ("0" + m.getUTCMinutes()).slice(-2) +
+      ("0" + m.getUTCSeconds()).slice(-2);
+    return dateString;
+  };
+  return (
+    <>
+      <audio ref={audioRef}></audio>
+      <h4 className="mb-4">
+        Website dựa trên{" "}
+        <a
+          href="https://viettelgroup.ai/service/tts"
+          rel="noreferrer"
+          target="_blank"
+        >
+          ViettelAI (API TTS - Text To Speech)
+        </a>
+      </h4>
+      <form onSubmit={convert}>
+        <FloatingLabel
+          controlId="floatingSelect1"
+          label="Chọn giọng đọc"
+          className="mb-3"
+        >
+          <Form.Select onChange={(e) => setVoice(e.target.value)} value={voice}>
+            {voices.map((v) => (
+              <option key={v.code} value={v.code}>
+                {v.name} - {v.description}
+              </option>
+            ))}
+          </Form.Select>
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingSelect2"
+          label="Chọn tốt độ đọc"
+          className="mb-3"
+        >
+          <Form.Select
+            onChange={(e) => setSpeed(Number(e.target.value))}
+            value={speed}
+          >
+            <option value={0.7}>-3</option>
+            <option value={0.8}>-2</option>
+            <option value={0.9}>-1</option>
+            <option value={1}>0</option>
+            <option value={1.1}>1</option>
+            <option value={1.2}>2</option>
+            <option value={1.3}>3</option>
+          </Form.Select>
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingTextarea"
+          label="Nhập nội dung"
+          className="mb-3"
+        >
+          <Form.Control
+            as="textarea"
+            placeholder="Nhập nội dung"
+            required
+            value={text}
+            maxLength={500}
+            onChange={(e) => setText(e.target.value)}
+            style={{ height: "100px" }}
+          />
+        </FloatingLabel>
+
+        <div className="d-flex">
+          {isLoading ? (
+            <button
+              type="button"
+              className="btn btn-primary d-flex align-items-center"
+            >
+              <Spinner animation="border" variant="light" /> &nbsp; Đang chuyển
+            </button>
+          ) : (
+            <button type="submit" className="btn btn-primary">
+              Chuyển
+            </button>
+          )}
+          {audioRef.current?.src && (
+            <>
+              <button
+                onClick={() => audioRef.current?.play()}
+                type="button"
+                className=" ms-4 btn btn-success"
+              >
+                Phát lại
+              </button>
+              <a
+                href={audioRef.current.src}
+                target="_blank"
+                rel="noreferrer"
+                download={`sound-${randomName()}.mp3`}
+                className=" ms-4 btn btn-warning"
+              >
+                Tải xuống âm thanh
+              </a>
+            </>
+          )}
+        </div>
+      </form>
+    </>
+  );
+};
